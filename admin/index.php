3bbc5ac79ee6a177bc2bdd1cbded3e142b790c78
ZiaKfa <zia14148@gmail.com>
2023-11-20 20:11:34 +0700

    menghapus plugin yang tidak digunakan pada adminlte

--- admin/index.php
+++ admin/index.php
@@ -378,9 +378,6 @@ a<script src="plugins/jquery-ui/jquery-ui.min.js"></script>
 </script>
 <!-- Bootstrap 4 -->
 <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<!-- ChartJS -->
-<!--<script src="plugins/chart.js/Chart.min.js"></script>-->
-<!-- Sparkline -->
 
 <!-- AdminLTE App -->
 <script src="dist/js/adminlte.min.js"></script>
